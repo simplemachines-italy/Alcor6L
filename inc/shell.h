d46e102f6c0f7b0657861ce18c45ad2c42dded9c
raman <ramangopalan@gmail.com>
2013-04-10 03:51:05 +0200

    Changed the default shell prompt to Alcor.

--- inc/shell.h
+++ inc/shell.h
@@ -1,12 +1,13 @@
-// eLua shell 
+// eLua shell - modified to support Alcor's
+// requirements.
 
 #ifndef __SHELL_H__
 #define __SHELL_H__
 
 #include "type.h"
 
-#define SHELL_WELCOMEMSG                "\neLua %s  Copyright (C) 2007-2011 www.eluaproject.net\n"
-#define SHELL_PROMPT                    "eLua# "
+#define SHELL_WELCOMEMSG                "\nAlcor6L %s  Copyright (C) simplemachines-italy\n"
+#define SHELL_PROMPT                    "alcor# "
 #define SHELL_ERRMSG                    "Invalid command, type 'help' for help\n"
 #define SHELL_MAXSIZE                   50
 #define SHELL_MAX_LUA_ARGS              8
