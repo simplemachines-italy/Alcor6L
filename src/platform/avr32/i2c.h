5f1caeb410f58901c221fb3c227cb097399c945a
raman <ramangopalan@gmail.com>
2013-06-24 14:20:34 +0200

    rename avr32's bool type to avr32_bool.
    This avoids conflict with the bool defined
    by PicoLisp.
    
    Note: This may or may not be the best way
    to go. When I just quickly looked up the
    places where avr32's bool was used, I only
    saw functions in the i2c block using this
    type. However, PicoLisp (which uses it as
    an enum type) has used this in several
    places.
    
    We have to get back to this if we have
    other bools defined in other Alcor6L targets.

--- src/platform/avr32/i2c.h
+++ src/platform/avr32/i2c.h
@@ -9,6 +9,6 @@ int i2c_write_byte( unsigned char byte );  // returns 0 if acked by slave
 unsigned char i2c_read_byte( int nack );
 
 // Higher-level interface
-int i2c_send(u8 slave_address, const u8 *data, unsigned len, bool stop);
-int i2c_recv(u8 slave_address, u8 *data, unsigned len, bool stop);
-bool i2c_probe(u8 slave_address);
+int i2c_send(u8 slave_address, const u8 *data, unsigned len, avr32_bool stop);
+int i2c_recv(u8 slave_address, u8 *data, unsigned len, avr32_bool stop);
+avr32_bool i2c_probe(u8 slave_address);
