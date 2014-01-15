e5e7f00840772f5cabe765444e691b7e3f7c6d37
raman <ramangopalan@gmail.com>
2014-01-15 15:55:13 +0100

    modules/auxmods.h: adjust symbol spacing

--- src/modules/auxmods.h
+++ src/modules/auxmods.h
@@ -45,19 +45,19 @@ LUALIB_API int ( luaopen_cpu )( lua_State* L );
 #define AUXLIB_ADC      "adc"
 LUALIB_API int ( luaopen_adc )( lua_State *L );
 
-#define AUXLIB_RPC   "rpc"
+#define AUXLIB_RPC      "rpc"
 LUALIB_API int ( luaopen_rpc )( lua_State *L );
 
 #define AUXLIB_BITARRAY "bitarray"
 LUALIB_API int ( luaopen_bitarray )( lua_State *L );
 
-#define AUXLIB_ELUA "elua"
+#define AUXLIB_ELUA     "elua"
 LUALIB_API int ( luaopen_elua )( lua_State *L );
 
-#define AUXLIB_I2C  "i2c"
+#define AUXLIB_I2C      "i2c"
 LUALIB_API int ( luaopen_i2c )( lua_State *L );
 
-#define AUXLIB_KS0108B "glcd"
+#define AUXLIB_KS0108B  "glcd"
 LUALIB_API int ( luaopen_glcd )( lua_State *L );
 
 // Helper macros
